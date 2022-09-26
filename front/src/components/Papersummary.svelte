a176fcea41a7804e80d99ef65294cdf59a239822
JMD <48573618+JDipi@users.noreply.github.com>
2022-09-25 22:19:40 -0700

    Add files via upload

---
+++ front/src/components/Papersummary.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let url, title, summary, related
+</script>
+
+<div class="flex flex-col text-gray-400 w-[98%] bg-neutral rounded-2xl my-4 p-4">
+  <a href={url} target="_blank" class="font-extrabold text-blue-500 break-words underline underline-offset-2">{title}</a>
+  <div class="divider divider-vertical bg-neutral my-2"></div>
+
+  {#if summary}
+    <p class="">{summary}</p>
+  {:else}
+    <p class="">loading summary</p>
+  {/if}
+
+  <div class="divider divider-vertical bg-neutral my-2"></div>
+
+  {#if summary}
+    <div class="flex flex-col sm:flex-row">
+      {#each related as similar, i}
+        <button class="btn grid mx-2 cursor-ointer flex-grow h-10 card bg-base-300 rounded-box place-items-center" on:click={() => window.open(`https://www.google.com/search?q=${similar}`,'_blank')}>{similar}</button>
+        {#if i+1 !== related.length}
+          <!-- <div class="divider my-1 dividerp-vertical sm:divider-horizontal sm:mx-1"></div>  -->
+        {/if}
+      {/each}
+    </div>
+  {:else}
+    <p class="">loading related</p>
+  {/if}
+  
+  
+</div>
---
+++ front/src/components/Searchbar.svelte
@@ -0,0 +1,30 @@
+<script>
+  import IoIosSearch from "svelte-icons/io/IoIosSearch.svelte";
+  import { createEventDispatcher } from 'svelte'
+  let val
+
+  const dispatch = createEventDispatcher()
+
+  function handlekey(e) {
+    if (e.keyCode === 13) {
+      dispatch('message',{text: 'Go', val})
+    }
+  }
+</script>
+
+
+<div
+  class="relative flex items-center w-full text-gray-400 focus-within:text-gray-400">
+  <input
+    autocomplete="off"
+    type="text"
+    placeholder="Enter Search Term"
+    class="input input-bordered bg-neutral w-full py-2 font-semibold placeholder-gray-400 rounded-xl"
+    bind:value={val} on:keydown={handlekey}/>
+  <button class="btn ml-4 border-gray-600 hover:border-secondary" on:click={() => {dispatch('message',{text: 'Go', val})}}>
+  <!-- <button class="btn ml-4 hover:border-secondary" on:click={message}> -->
+    <span class="w-7">
+      <IoIosSearch />
+    </span>
+  </button>
+</div>
