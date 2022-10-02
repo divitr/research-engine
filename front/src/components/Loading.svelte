ca79d59e158b0416fecdc9ba847ceaa3c56bb557
Divit Rawal <58348184+divitr@users.noreply.github.com>
2022-10-02 15:24:46 -0700

    Delete front directory

--- front/src/components/Loading.svelte
+++
@@ -1,3 +0,0 @@
-<div class="w-3/4 h-52 rounded-2xl bg-gray-600 m-10 flex items-center justify-center">
-  <p>cool fact</p>
-</div>
--- front/src/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-export const term = writable('')
-
-export const pagetexts = writable()
