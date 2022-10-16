bf8cd72e2dd17aec92ac78e4e5f77188089aa85f
Divit Rawal <58348184+divitr@users.noreply.github.com>
2022-10-15 20:49:50 -0700

    Delete version3/client/src/src directory

--- version3/client/src/src/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-export const term = writable('')
-
-export const pagetexts = writable()
-
-export const query = writable()
-
-export const relaventArticles = writable()
