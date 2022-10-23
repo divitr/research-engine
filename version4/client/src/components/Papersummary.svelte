435e15b0d51a64136f3b521f818a17ae9dd4b299
JMD <48573618+JDipi@users.noreply.github.com>
2022-10-22 22:33:35 -0700

    Update Papersummary.svelte

--- version4/client/src/components/Papersummary.svelte
+++ version4/client/src/components/Papersummary.svelte
@@ -1,6 +1,8 @@
 <script>
   export let url, title, summary, related, text
+  let lines = text.split('.').slice(0,4)
 </script>
+
 
 <div class="flex flex-col text-gray-400 w-[98%] bg-neutral rounded-2xl my-4 p-4">
   <a href={url} target="_blank" class="font-extrabold text-blue-500 break-words underline underline-offset-2">{title}</a>
@@ -9,7 +11,8 @@
   {#if summary}
     <p class="">{summary}</p>
   {:else}
-    <p class="">{text}</p>
+    <p class="font-bold text-lg">Loading summary...<br><br></p>
+    <p class="">{lines}</p>
   {/if}
 
   <div class="divider divider-vertical bg-neutral my-2"></div>
@@ -18,14 +21,9 @@
     <div class="flex flex-col xl:flex-row">
       {#each related as similar, i}
         <button class="btn grid mx-2 cursor-ointer flex-grow h-10 card bg-base-300 rounded-box place-items-center" on:click={() => window.open(`https://www.google.com/search?q=${similar}`,'_blank')}>{similar}</button>
-        {#if i+1 !== related.length}
-          <!-- <div class="divider my-1 dividerp-vertical sm:divider-horizontal sm:mx-1"></div>  -->
-        {/if}
       {/each}
     </div>
   {:else}
-    <p class="">Loading related...</p>
+    <p class="font-bold text-lg">Loading related terms...</p>
   {/if}
-  
-  
 </div>
